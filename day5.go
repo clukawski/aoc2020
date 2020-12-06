291e30d82e6bde5769319e71bbbe2711259d4d54
Conrad Lukawski <conrad@backlight.ca>
2020-12-05 23:23:57 -0500

    better day 5

--- day5.go
+++ day5.go
@@ -4,8 +4,11 @@ import (
 	"fmt"
 	"io/ioutil"
 	"sort"
+	"strconv"
 	"strings"
 )
+
+// Note: this is the my second implementation, go back in history to see previous, uglier solution, thanks to @benharri for inspo
 
 const rows = 128
 
@@ -13,55 +16,11 @@ func day5s1() {
 	file, _ := ioutil.ReadFile("./input5")
 	input := strings.Split(string(file), "\n")
 
-	var rows []int
 	var seatIDs []int
-	for i := 0; i < 128; i++ {
-		rows = append(rows, i+1)
-	}
-
-	var columns []int
-	for i := 0; i < 8; i++ {
-		columns = append(columns, i+1)
-	}
-
 	for _, pass := range input {
-		rowrange := rows
-		colrange := columns
-		parts := strings.Split(pass, "")[:7]
-
-		for _, part := range parts {
-			if len(rowrange) == 1 {
-				break
-			}
-			half := len(rowrange) / 2
-			switch part {
-			case "F":
-				rowrange = rowrange[:half]
-			case "B":
-				rowrange = rowrange[half:]
-			}
-			fmt.Printf("rowrange (%s): %v\n", part, rowrange)
-		}
-
-		parts2 := strings.Split(pass, "")[7:]
-		for _, part := range parts2 {
-			if len(colrange) == 1 {
-				break
-			}
-			half := len(colrange) / 2
-
-			switch part {
-			case "L":
-				colrange = colrange[:half]
-			case "R":
-				colrange = colrange[half:]
-			}
-			fmt.Printf("colrange (%s): %v\n", part, colrange)
-		}
-
-		seatIDs = append(seatIDs, (rowrange[0]-1)*8+(colrange[0]-1))
+		id := getSeatID(pass)
+		seatIDs = append(seatIDs, int(id))
 	}
-
 	sort.Ints(seatIDs)
 
 	fmt.Printf("list: %v\n", seatIDs)
@@ -72,85 +31,29 @@ func day5s2() {
 	file, _ := ioutil.ReadFile("./input5")
 	input := strings.Split(string(file), "\n")
 
-	var rows []int
 	var seatIDs []int
-	var seatMap = make(map[struct {
-		Row    int
-		Column int
-	}]int)
-	var seatIDMap = make(map[int][]int)
-	for i := 0; i < 128; i++ {
-		rows = append(rows, i+1)
-	}
-
-	var columns []int
-	for i := 0; i < 8; i++ {
-		columns = append(columns, i+1)
-	}
-
 	for _, pass := range input {
-		rowrange := rows
-		colrange := columns
-		parts := strings.Split(pass, "")[:7]
-
-		for _, part := range parts {
-			if len(rowrange) == 1 {
-				break
-			}
-			half := len(rowrange) / 2
-			switch part {
-			case "F":
-				rowrange = rowrange[:half]
-			case "B":
-				rowrange = rowrange[half:]
-			}
-		}
-
-		parts2 := strings.Split(pass, "")[7:]
-		for _, part := range parts2 {
-			if len(colrange) == 1 {
-				break
-			}
-			half := len(colrange) / 2
-			// half := (len(colrange) / 2) + (len(colrange) % 2)
-			switch part {
-			case "L":
-				colrange = colrange[:half]
-			case "R":
-				colrange = colrange[half:]
-			}
-		}
-
-		id := (rowrange[0]-1)*8 + (colrange[0] - 1)
-		seatIDs = append(seatIDs, id)
-		seatMap[struct {
-			Row    int
-			Column int
-		}{rowrange[0] - 1, colrange[0] - 1}] = id
-		seatIDMap[id] = []int{rowrange[0] - 1, colrange[0] - 1}
+		id := getSeatID(pass)
+		seatIDs = append(seatIDs, int(id))
 	}
-
 	sort.Ints(seatIDs)
 
-	var rownums []int
-	for _, v := range seatIDMap {
-		rownums = append(rownums, v[0])
-	}
-	sort.Ints(rownums)
-	uniqueRows := unique(rownums)
-	var mySeat int
-	for _, row := range uniqueRows[1 : len(uniqueRows)-1] {
-		for i := 0; i < 8; i++ {
-			if _, ok := seatMap[struct {
-				Row    int
-				Column int
-			}{Row: row, Column: i}]; !ok {
-				mySeat = row*8 + i
-			}
-		}
-	}
+	seat := (len(seatIDs)+1)*(seatIDs[0]+seatIDs[len(seatIDs)-1])/2 - sum(seatIDs)
+	fmt.Printf("my seat: %d\n", seat)
+}
 
-	fmt.Printf("my seat: %d\n", mySeat)
+func getSeatID(input string) int64 {
+	mapReplace := func(r rune) rune {
+		switch r {
+		case 'F', 'L':
+			return '0'
+		case 'B', 'R':
+			return '1'
+		}
+		return 'x' // invalid
+	}
+	i2, _ := strconv.ParseInt(strings.Map(mapReplace, input), 2, 64)
+	return i2
 }
 
 func unique(slice []int) []int {
@@ -164,4 +67,12 @@ func unique(slice []int) []int {
 	}
 
 	return valid
+}
+
+func sum(array []int) int {
+	result := 0
+	for _, v := range array {
+		result += v
+	}
+	return result
 }
