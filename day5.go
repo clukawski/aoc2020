3176bc8501bf796d83ff9f0328dcf8a77f33aee5
Conrad Lukawski <conrad@backlight.ca>
2020-12-05 23:27:15 -0500

    even better

--- day5.go
+++ day5.go
@@ -13,21 +13,17 @@ import (
 const rows = 128
 
 func day5s1() {
-	file, _ := ioutil.ReadFile("./input5")
-	input := strings.Split(string(file), "\n")
-
-	var seatIDs []int
-	for _, pass := range input {
-		id := getSeatID(pass)
-		seatIDs = append(seatIDs, int(id))
-	}
-	sort.Ints(seatIDs)
-
-	fmt.Printf("list: %v\n", seatIDs)
+	seatIDs := *getSeatIDList()
 	fmt.Printf("highest: %d\n", seatIDs[len(seatIDs)-1])
 }
 
 func day5s2() {
+	seatIDs := *getSeatIDList()
+	seat := (len(seatIDs)+1)*(seatIDs[0]+seatIDs[len(seatIDs)-1])/2 - sum(seatIDs)
+	fmt.Printf("my seat: %d\n", seat)
+}
+
+func getSeatIDList() *[]int {
 	file, _ := ioutil.ReadFile("./input5")
 	input := strings.Split(string(file), "\n")
 
@@ -38,8 +34,7 @@ func day5s2() {
 	}
 	sort.Ints(seatIDs)
 
-	seat := (len(seatIDs)+1)*(seatIDs[0]+seatIDs[len(seatIDs)-1])/2 - sum(seatIDs)
-	fmt.Printf("my seat: %d\n", seat)
+	return &seatIDs
 }
 
 func getSeatID(input string) int64 {
