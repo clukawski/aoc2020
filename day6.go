a5bc47ac1c68387cc2d0a0e6d6982f49b25d57dd
Conrad Lukawski <conrad@backlight.ca>
2020-12-06 01:43:26 -0500

    Day 6

--- day6.go
+++ day6.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"math"
 	"strings"
 )
 
@@ -27,4 +28,40 @@ func day6s1() {
 		groupsAns = append(groupsAns, total)
 	}
 	fmt.Println(sum(groupsAns))
+}
+
+func day6s2() {
+	file, _ := ioutil.ReadFile("./input6")
+	groups := strings.Split(string(file), "\n\n")
+	var groupsAns []int
+	for _, group := range groups {
+		ans := []string{}
+		persons := strings.Split(group, "\n")
+		for i, person := range persons {
+			var ansPerson []string
+			answers := strings.Split(person, "")
+			ansPerson = append(ansPerson, answers...)
+
+			if i == 0 {
+				ans = ansPerson
+				continue
+			}
+
+			ans = intersect(ans, ansPerson)
+		}
+		groupsAns = append(groupsAns, len(ans))
+	}
+	fmt.Println(sum(groupsAns))
+}
+
+func intersect(as, bs []string) []string {
+	i := make([]string, 0, int(math.Max(float64(len(as)), float64(len(bs)))))
+	for _, a := range as {
+		for _, b := range bs {
+			if a == b {
+				i = append(i, a)
+			}
+		}
+	}
+	return i
 }
